2d42ed0c930c1919b95e1a75776cc71edb062e59
Nicolas Botelho <nicolasbotelho2005@gmail.com>
2025-05-19 14:06:44 -0300

    Update Infrastructure

--- src/cli/backend/csharp/clean-architecture/Infrastructure/Repositories/generator.ts
+++ src/cli/backend/csharp/clean-architecture/Infrastructure/Repositories/generator.ts
@@ -12,7 +12,6 @@ export function generate(model: Model, target_folder: string) : void {
     fs.mkdirSync(common_folder, {recursive: true})
     fs.mkdirSync(entities_folder, {recursive: true})
 
-    fs.writeFileSync(path.join(common_folder, `BaseRepository.cs`), generateBase(model))
     fs.writeFileSync(path.join(common_folder, `UnitOfWork.cs`), generateUnitOfWork(model))
     const modules =  model.abstractElements.filter(isModule);
   
@@ -23,74 +22,9 @@ export function generate(model: Model, target_folder: string) : void {
     }
 }
 
-function generateBase (model: Model): string {
-    return expandToStringWithNL`
-using ${model.configuration?.name}.Domain.Common;
-using ${model.configuration?.name}.Domain.Interfaces.Common;
-using ${model.configuration?.name}.Infrastructure.Context;
-using Microsoft.EntityFrameworkCore;
-
-namespace ${model.configuration?.name}.Infrastructure.Repositories.Common
-{
-    public class BaseRepository<T> : IBaseRepository<T> where T : BaseEntity
-    {
-        protected readonly AppDbContext Context;
-        protected readonly DbSet<T> DbSet;
-
-        public BaseRepository(AppDbContext context)
-        {
-            Context = context;
-            DbSet =  Context.Set<T>();
-        }
-
-        public async Task Create(T entity)
-        {
-            await Context.AddAsync(entity);
-        }
-
-        public async Task Delete(T entity)
-        {
-            entity.Delete();
-            await Task.Run(() => Context.Remove(entity), new CancellationToken());
-        }
-
-        public async Task Update(T entity)
-        {
-            await Task.Run(() => Context.Update(entity), new CancellationToken());
-        }
-
-        public IQueryable<T> GetAll()
-        {
-            return Context.Set<T>()
-                 .AsSplitQuery().AsQueryable();
-        }
-
-        public IQueryable<T> GetById(Guid id)
-        {
-            return Context.Set<T>().Where(x => x.Id == id).AsNoTracking().AsQueryable();
-        }
-
-        public async Task<bool> Any(Guid id)
-        {
-            return await Context.Set<T>().AnyAsync(x => x.Id.Equals(id));
-        }
-
-        public void DeleteRange(ICollection<T> entities)
-        {
-            Context.Set<T>().RemoveRange(entities);
-        }
-
-        public void AddRange(ICollection<T> entities)
-        {
-            Context.Set<T>().AddRange(entities);
-        }
-    }
-}`
-}
-
 function generateUnitOfWork(model: Model) : string {
     return expandToStringWithNL`
-using ${model.configuration?.name}.Domain.Interfaces.Common;
+using ConectaFapes.Common.Infrastructure.Interfaces;
 using ${model.configuration?.name}.Infrastructure.Context;
 
 namespace ${model.configuration?.name}.Infrastructure.Repositories
@@ -118,7 +52,7 @@ function generateEntityRepository(model: Model, cls: LocalEntity): string {
 using ${model.configuration?.name}.Domain.Enums;
 using ${model.configuration?.name}.Domain.Interfaces.Entities;
 using ${model.configuration?.name}.Infrastructure.Context;
-using ${model.configuration?.name}.Infrastructure.Repositories.Common;
+using ConectaFapes.Common.Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 namespace ${model.configuration?.name}.Infrastructure.Repositories.Entities
